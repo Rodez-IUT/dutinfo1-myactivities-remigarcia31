5346aa12566fc23117159863154127d83053abaf
Rémi Garcia <remi.garcia@iut-rodez.fr>
2019-03-20 14:18:02 +0100

    GIT Fix 4.3 function to fix activities without owner

--- src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -22,3 +22,19 @@ BEGIN
 END;
 
 $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION fix_activities_without_owner() RETURNS SETOF activity AS $$
+DECLARE
+
+	
+BEGIN
+
+	UPDATE activity
+	SET owner_id = (SELECT id FROM "user" WHERE username = 'Default Owner')
+	WHERE owner_id IS NULL;
+	
+	RETURN QUERY SELECT * FROM activity WHERE owner_id = (SELECT id FROM "user" WHERE username = 'Default Owner') ;
+	RETURN;
+END;
+$$ LANGUAGE plpgsql;
